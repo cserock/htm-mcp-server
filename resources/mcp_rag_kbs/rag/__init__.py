ec683447686f035178e402aca10b01d7caaf32c3
rock-tumblbug <rock@backpac.kr>
2025-09-04 19:01:11 +0900

    Delete not used file

--- resources/mcp_rag_kbs/rag/__init__.py
+++ resources/mcp_rag_kbs/rag/__init__.py
@@ -1,11 +1,9 @@
 from rag.base import RetrievalChain
 from rag.pdf import PDFRetrievalChain
-from rag.md import MDRetrievalChain
 from rag.kbs import KBSRetrievalChain
 
 __all__ = [
     'RetrievalChain',
     'PDFRetrievalChain',
-    'MDRetrievalChain',
     'KBSRetrievalChain'
 ]
